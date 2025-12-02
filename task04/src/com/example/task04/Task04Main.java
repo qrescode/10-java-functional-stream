4f54616313741f28c5e34eba3d005513ebcd8d72
nikita <nikitakosarev1@list.ru>
2025-12-02 08:54:51 +0500

    mod 10

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -3,42 +3,32 @@ package com.example.task04;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.function.Function;
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
-import java.util.Map.Entry;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Task04Main {
 
     public static void main(String[] args) {
-
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in, StandardCharsets.UTF_8)
         );
 
-        String text = reader.lines()
-                .collect(Collectors.joining(" "));
-
-        String[] words = text.toLowerCase(Locale.ROOT)
-                .split("[^A-Za-zА-Яа-яЁё0-9]+");
-
-        Map<String, Long> freq = Arrays.stream(words)
+        reader.lines()
+                .map(s -> s.toLowerCase())
+                .flatMap(s -> Stream.of(s.split("[^A-Za-zА-Яа-яЁё0-9]+")))
                 .filter(s -> !s.isEmpty())
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        Collectors.counting()
-                ));
-
-        List<String> topWords = freq.entrySet().stream()
-                .sorted(
-                        Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
-                                .thenComparing(Entry.comparingByKey())
-                )
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .sorted(Comparator
+                        .comparingLong((ToLongFunction<Map.Entry<String, Long>>) Map.Entry::getValue)
+                        .reversed()
+                        .thenComparing(Map.Entry::getKey))
                 .limit(10)
-                .map(Entry::getKey)
-                .collect(Collectors.toList());
-
-        System.out.print(String.join("\n", topWords));
+                .forEach(e -> System.out.println(e.getKey()));
     }
 }
